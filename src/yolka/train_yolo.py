8365dd58516994d238b210d4e85a767b025e03a9
Eidzoku Makura <106468562+eidzokumakura@users.noreply.github.com>
2024-03-13 19:59:56 +0300

    yolo

---
+++ src/yolka/train_yolo.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+yaml_file = 'dataset/dataset.yaml'
+
+# Load a model
+model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+
+
+if __name__ == '__main__':
+    # Train the model
+    results = model.train(data=yaml_file, epochs=50, imgsz=640, batch=8)
--- src/yolka/yolov8.py
+++ src/yolka/yolov8.py
@@ -1,4 +1,3 @@
-import cv2
 from ultralytics import YOLO
 
 # Load a model
